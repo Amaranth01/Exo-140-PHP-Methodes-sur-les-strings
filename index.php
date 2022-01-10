7e50968ff608b89f466fea5e65fa7727abd1f007
Amaranth01 <vanessa.nectoux@gmail.com>
2022-01-10 16:08:35 +0100

    Exo 140 ok

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
 <?php
 
 // Compléter le code ci dessous pour afficher la longueur de la chaine de caracteres
+$chaine = ("google");
+echo  strlen($chaine);
 
-echo      ("google");
+echo"<br><br>"; //Decommentez les <br> pour tester l'affichage lorsque vous aurez plusieurs instructions
 
-//echo"<br><br>"; //Decommentez les <br> pour tester l'affichage lorsque vous aurez plusieurs instructions
-
-// Compléter le code ci dessous pour inverser la chaine de caractere
-
-echo  ("anticonstitutionellement");
-
+// Compléter le code ci-dessous pour inverser la chaine de caractere
+$chaine2 = ("anticonstitutionellement");
+echo  strrev($chaine2);
+echo"<br><br>";
 // Et si vous testiez également avec votre prénom ?
 
+$prenom = ("Vanessa");
+echo  strrev($prenom);
 
-
-//echo"<br><br>";
+echo"<br><br>";
 
 // Compléter le code ci dessous pour remplacer le mot ok par non
 $oldtxt = "Ok Google!";
-$newtxt = (, , );
+$newtxt = str_replace ("Ok", "non", "Ok Google");
 
 echo $newtxt;
